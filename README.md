2cb67e70e3a3c45b4d63154d0809c556b48e5952
liyang <liyang@wikiflyer.cn>
2022-02-22 14:19:17 +0800

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# vue2-wordle
+
+[Wordle 游戏](https://www.powerlanguage.co.uk/wordle/)的 Vue2 实现。
+
+看到尤雨溪大佬的搞了一个 vue3 的项目，感觉挺有意思的就自己也搞一个 vue2 版本的。
+
+该项目仅为了娱乐和学习。
---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Keyboard />
+  </div>
+</template>
+
+<script>
+import Keyboard from './Keyboard.vue'
+export default {
+  name: 'App',
+  components: {
+    Keyboard
+  }
+}
+</script>
+
+<style>
+#app {
+}
+</style>
